4827c4c9e1f0a75ccdae23e4b03da49c55139ad0
Abdulaziz1268 <abdumh1268@gmail.com>
2025-03-27 11:24:55 +0300

    checking for eas update

--- doctor-appointment/App.js
+++ doctor-appointment/App.js
@@ -1,4 +1,4 @@
-import { Alert, StyleSheet, Text, View } from "react-native"
+import { StyleSheet } from "react-native"
 import AppSafeAreaView from "./app/components/AppSafeAreaView"
 import AppButton from "./app/components/AppButton"
 import AppTextInput from "./app/components/AppTextInput"
@@ -7,42 +7,12 @@ import RegisterScreen from "./app/screens/RegisterScreen"
 import DashboardScreen from "./app/screens/Doctor/DashboardScreen"
 import { NavigationContainer } from "@react-navigation/native"
 import { DoctorTabNavigator } from "./app/routes/DoctorRoutes"
-import { useContext, useEffect } from "react"
+import { useContext } from "react"
 import { AuthContext, AuthProvider } from "./app/Contexts/AuthContext"
 import AuthStackNavigator from "./app/routes/AuthRoutes"
 import { PrefProvider } from "./app/Contexts/PrefContext"
-import * as updates from "expo-updates"
 
 export default function App() {
-  const checkForUpdates = async () => {
-    try {
-      const update = await updates.checkForUpdateAsync()
-      if (update.isAvailable) {
-        Alert.alert("Update Available!", "Do you want to update now?", [
-          { text: "Later", style: "cancel" },
-          {
-            text: "Update",
-            onPress: async () => {
-              try {
-                await updates.fetchUpdateAsync()
-                await updates.reloadAsync()
-              } catch (error) {
-                Alert.alert("Update failed!", "please try again later.")
-                console.log(error)
-              }
-            },
-          },
-        ])
-      }
-    } catch (error) {
-      console.log("Error checking for updates", error)
-    }
-  }
-
-  useEffect(() => {
-    checkForUpdates()
-  }, [])
-
   return (
     <AppSafeAreaView>
       <PrefProvider>
--- doctor-appointment/app/screens/Doctor/ProfileScreen.js
+++ doctor-appointment/app/screens/Doctor/ProfileScreen.js
@@ -1,20 +1,65 @@
-import React from "react"
-import { StyleSheet, View, Text } from "react-native"
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native"
 import colors from "../../config/colors"
+import * as updates from "expo-updates"
+import Toast from "react-native-toast-message"
 
 const ProfileScreen = () => {
+  const checkForUpdates = async () => {
+    try {
+      const update = await updates.checkForUpdateAsync()
+      if (update.isAvailable) {
+        Alert.alert("Update Available!", "Do you want to update now?", [
+          { text: "Later", style: "cancel" },
+          {
+            text: "Update",
+            onPress: async () => {
+              try {
+                await updates.fetchUpdateAsync()
+                await updates.reloadAsync()
+                Toast.show({ type: "success", text1: "successfully updated" })
+              } catch (error) {
+                Alert.alert("Update failed!", "please try again later.")
+                console.log(error)
+                Toast.show({ type: "error", text1: "update failed" })
+              }
+            },
+          },
+        ])
+      }
+    } catch (error) {
+      console.log("Error checking for updates", error)
+      Toast.show({ type: "error", text1: "error checking for updates" })
+    }
+  }
   return (
     <View style={styles.profileContainer}>
-      <Text>Profile</Text>
+      <Text style={styles.header}>Profile</Text>
+      <TouchableOpacity onPress={checkForUpdates} style={styles.updates}>
+        <Text>Check for update</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  header: {
+    fontSize: 28,
+    marginLeft: "10%",
+    marginTop: "5%",
+    marginBottom: 15,
+    fontWeight: "bold",
+    color: colors.blue,
+  },
   profileContainer: {
     backgroundColor: colors.lightblue,
     height: "100%",
     width: "100%",
+  },
+  updates: {
+    backgroundColor: colors.white,
+    height: 50,
+    justifyContent: "center",
+    paddingLeft: 20,
   },
 })
 
